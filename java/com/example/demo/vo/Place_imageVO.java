6b37755fd767feda7f34c8f969bdb49e932b6f7f
Yana Hwang <46346904+yanapang@users.noreply.github.com>
2022-06-10 18:23:53 +0900

    room, user_reservation, place_image VO file upload

---
+++ java/com/example/demo/vo/Place_imageVO.java
@@ -0,0 +1,7 @@
+package com.example.demo.vo;
+
+public class Place_imageVO {
+	private int IMAGE_NUM;
+    private int PLACE_NUM;
+    private String PLACE_FILE;
+}
---
+++ java/com/example/demo/vo/RoomVO.java
@@ -0,0 +1,8 @@
+package com.example.demo.vo;
+
+public class RoomVO {
+	 private int ROOM_NUM;
+	 private String ROOM_NAME;
+	 private int ROOM_PRICE;
+	 private int PLACE_NUM;
+}
