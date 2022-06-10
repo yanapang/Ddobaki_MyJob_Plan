21fdbede656e17bf9a9f60e55c681d5f8844c440
juheeko8524 <106743527+juheeko8524@users.noreply.github.com>
2022-06-10 18:21:36 +0900

    dog, reply VO 파일 업로드
    dog, reply VO 작성

---
+++ java/com/example/demo/vo/DogVO.java
@@ -0,0 +1,14 @@
+package com.example.demo.vo;
+
+import lombok.Data;
+
+@Data
+public class DogVO {
+	private int user_num;
+	private int dog_num;
+	private String dog_name;
+	private int dog_age;
+	private int dog_gender;
+	private int dog_desexed;
+	private int dog_weight;
+}
